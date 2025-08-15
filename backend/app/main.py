cab844d87b8e2a468317205d53acb92142f371b9
Sérgio Adriano <sergioadrianomc@gmail.com>
2025-08-15 12:00:40 -0300

    Implementação do gráfico, cabeçalho e body

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
-from app.api import charts
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.charts import router as charts_router
 
 app = FastAPI()
-app.include_router(charts.router)
+
+#Libera acesso do frontend
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+app.include_router(charts_router)
